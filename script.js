6698ee3623aaafa59386824c454050b27ff57a3c
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2022-08-09 21:05:34 +0200

    add docker

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
-const fetch = require("node-fetch"),
-    { execFile } = require("child_process"),
+const { execFile } = require("child_process"),
     express = require('express')(),
     port = 15000,
     { readFileSync } = require("fs");
@@ -12,26 +11,15 @@ const fetch = require("node-fetch"),
         "--authtoken=" + process.env.NGROK_AUTH_TOKEN,
         "--config=.\\ngrok.yml"
     ]);
-    console.log("Waiting for ngrok to start...");
-    const ngrokTunnels = await fetch("https://api.ngrok.com/tunnels", {
-        method: "GET",
-        headers: {
-            "Authorization": "Bearer " + process.env.NGROK_API_KEY,
-            "Ngrok-Version": "2"
-        }
-    }).then(async r => await r.json());
-
-    console.log(ngrokTunnels.tunnels.length, "tunnels");
+    console.log("https://dashboard.ngrok.com/cloud-edge/endpoints");
 
     express.get('/', (req, res) => {
-        res.redirect(ngrokTunnel.public_url);
-    });
-
-    express.get('/log', (req, res) => {
         res.send(readFileSync(".\\ngrok.log", "utf8"));
     });
 
     express.listen(port, () => {
         console.log('Express server listening on port '+port);
     });
+
+    execFile(`docker-compose`, ["up"]);
 })();
